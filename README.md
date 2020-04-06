b0d84a65f8c8bcc53319b9959433f315340bb1c7
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-04-06 14:27:28 +0300

    👶 Basic input organization

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # 🛍️ OpenImages Tool
 A tool to make OpenImages data Tensorflow-friendly
+
+## ⚠️ Required File Organization
+```
+Dataset
+|___ validation
+|    |___ <object_name>
+|          |___ Label
+|          |    |___ <validation_label_file_name>.txt
+|          |    |___ ....
+|          |___ <validation_image_file_name>.jpg
+|          |___ ....
+|
+|___ test
+|    |___ <object_name>
+|          |___ Label
+|          |    |___ <validation_label_file_name>.txt
+|          |    |___ ....
+|          |___ <validation_image_file_name>.jpg
+|          |___ ....
+|
+|___ train
+     |___ <object_name>
+           |___ Label
+           |    |___ <validation_label_file_name>.txt
+           |    |___ ....
+           |___ <validation_image_file_name>.jpg
+           |___ ....
+
+```
+> 📢 validation, test, train and Label are required keywords
+
+## 🎉 Result File Organization
+```
+images
+|___ test
+|    |___ <object_name_1>_test.xml
+|    |___ <object_name_1>_test.jpg
+|    |___ ....
+|
+|___ train
+     |___ <object_name_1>_train.xml
+     |___ <object_name_1>_train.jpg
+     |___ ....
+
+```
---
+++ src/script.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+if __name__ == "__main__":
+    # creating argument parser
+    parser = argparse.ArgumentParser(
+        description='OpenImages to TensorFlow format converter')
+
+    # adding arguments
+    parser.add_argument('-i',
+                        '--input_dir',
+                        help='Path to input OpenImages directory',
+                        type=str,
+                        required=True)
+    parser.add_argument('-o',
+                        '--output_dir',
+                        help='Path to input OpenImages directory',
+                        type=str,
+                        required=True)
+    parser.add_argument('-v',
+                        '--validation',
+                        help='Add validation images to train directory (boolean), default',
+                        default=True,
+                        type=float)
+    args = parser.parse_args()
+
+    assert os.path.isdir(os.path.dirname(
+        args.input_dir)), 'ERROR: No such input directory {} '.format(args.input_dir)
+    assert os.path.isdir(os.path.dirname(
+        args.output_dir)), 'ERROR: No such output directory {} '.format(args.output_dir)
+
+    main(args)
